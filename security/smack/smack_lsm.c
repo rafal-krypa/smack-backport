357d42bbabaf9458cee96b10197bc12f226e9dae
Konstantin Khlebnikov <k.khlebnikov@samsung.com>
2014-08-07 20:52:49 +0400

    BACKPORT: Smack: remove unneeded NULL-termination from securtity label
    Values of extended attributes are stored as binary blobs. NULL-termination
    of them isn't required. It just wastes disk space and confuses command-line
    tools like getfattr because they have to print that zero byte at the end.
    
    This patch removes terminating zero byte from initial security label in
    smack_inode_init_security and cuts it out in function smack_inode_getsecurity
    which is used by syscall getxattr. This change seems completely safe, because
    function smk_parse_smack ignores everything after first zero byte.
    
    Signed-off-by: Konstantin Khlebnikov <k.khlebnikov@samsung.com>
    (cherry-picked from upstream da1b63566c469bf3e2b24182114422e16b1aa34c)

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -675,7 +675,7 @@ static int smack_inode_init_security(struct inode *inode, struct inode *dir,
 	}
 
 	if (len)
-		*len = strlen(isp) + 1;
+		*len = strlen(isp);
 
 	return 0;
 }
@@ -1079,7 +1079,7 @@ static int smack_inode_getsecurity(const struct inode *inode,
 
 	if (strcmp(name, XATTR_SMACK_SUFFIX) == 0) {
 		isp = smk_of_inode(inode);
-		ilen = strlen(isp) + 1;
+		ilen = strlen(isp);
 		*buffer = isp;
 		return ilen;
 	}
@@ -1104,7 +1104,7 @@ static int smack_inode_getsecurity(const struct inode *inode,
 	else
 		return -EOPNOTSUPP;
 
-	ilen = strlen(isp) + 1;
+	ilen = strlen(isp);
 	if (rc == 0) {
 		*buffer = isp;
 		rc = ilen;
